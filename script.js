b8403eacbe42f73a6fdb5c76217edd08982caf12
Aurora <ada10016@yahoo.com>
2021-06-02 19:01:34 -0400

    function not functioning

---
+++ script.js
@@ -0,0 +1,35 @@
+var input = document.querySelectorAll('input');
+var btn = document.querySelector("#add");
+var para = document.querySelectorAll("p");
+
+para[0].style.fontSize = "54px";
+para[0].style.color = "red";
+
+
+let arr = [];
+
+
+//   function showArr() {
+//     for(let i = 0; i < arr.length; i++) {
+//       let array = inputs.value;
+
+//           array.push(arr);
+//           array.append(arr)
+
+//           para[1].textContent = `{input}.value`;     
+
+//   }
+//  } 
+
+// //==== or ====//
+
+
+// btn.addEventListener('click', function(){
+//   const input = document.querySelector('input').value;
+//       document.querySelectorAll("input")[1].value;
+//       document.querySelectorAll("input")[2].value;
+//       document.querySelectorAll("input")[3].value;
+//       document.querySelectorAll("input")[4].value;
+ 
+//     para[1].textContent = arr.value;
+// })
