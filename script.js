0d4568ff4496e0d91e4d68d0024daf2f9fb94c15
Aurora <ada10016@yahoo.com>
2021-06-03 16:30:20 -0400

    fixing input to html

--- script.js
+++ script.js
@@ -12,22 +12,10 @@ let arr = [];
     var input = document.querySelectorAll("input");
     for(let i = 0; i < input.length; i++) {
       let value = input[i].value;
-        value.append(para[1].)
-          
-        textContent = `${arr.value}`;     
+        para[1].append(value);
+
 
   }
  } 
 
-// //==== or ====//
-
-
-// btn.addEventListener('click', showArr(){
-//   const input = document.querySelector ('input').value;
-//       document.querySelectorAll("input")[1].value;
-//       document.querySelectorAll("input")[2].value;
-//       document.querySelectorAll("input")[3].value;
-//       document.querySelectorAll("input")[4].value;
- 
-//     para[1].textContent = arr.value;
-// })
+btn.addEventListener('click', showArr)
