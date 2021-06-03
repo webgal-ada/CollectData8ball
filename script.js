55d1226c0880ce842892d12af46bbefd1978a5ac
Aurora <ada10016@yahoo.com>
2021-06-03 18:57:01 -0400

    paragraph accepting input in one long line only:(

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ let arr = [];
 
   function showArr() {
     var input = document.querySelectorAll("input");
-    for(let i = 0; i < input.length; i++) {
-      input[i].value = '';
+    for(let i = 0; i < input.length; i++) {     
       let value = input[i].value;
-        para[1].append(value + " ");
+      input[i].value = "";
+      
+      document.querySelectorAll("p")[1].append(value, " ");
 
 
   }
  } 
-
 btn.addEventListener('click', showArr)
